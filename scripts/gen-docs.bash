50f2c660ef5fec03536f8f151a99a36ff22a1e64
Claude Falguiere <cfalguiere@gmail.com>
2014-05-08 23:56:04 +0200

    ajout des répertoires logistique et presse

--- scripts/gen-docs.bash
+++ scripts/gen-docs.bash
@@ -5,13 +5,14 @@ GHCSS2=https://devoxx4kidsfr.github.io/materials/stylesheets/pygment_trac.css
 HEADER="--include-before-body=$(dirname $0)/header.html"
 HTML_OPTIONS="--from=markdown --to=html5 --standalone --css $GHCSS1 --css $GHCSS2 $HEADER"
 
-for mdfile in $(find ./ateliers -name "*.md" -anewer $(dirname $0)/gen-docs-timestamp)
+for folder in "ateliers" "logistique" "presse-medias"
 do
-    echo $mdfile
-   pandoc $mdfile $HTML_OPTIONS --output=$(dirname $mdfile)/$(basename $mdfile .md).html
+    for mdfile in $(find $folder -name "*.md" -anewer $(dirname $0)/gen-docs-timestamp)
+    do
+	echo $mdfile
+	pandoc $mdfile $HTML_OPTIONS --output=$(dirname $mdfile)/$(basename $mdfile .md).html
+    done
 done
-
-
 
 touch $(dirname $0)/gen-docs-timestamp
 
