b80e7828def6c60f16f13676ba34b9915ed51d27
Claude Falguiere <cfalguiere@gmail.com>
2014-05-08 21:23:10 +0200

    ajout des CSS

--- scripts/gen-docs.bash
+++ scripts/gen-docs.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+GHCSS1=devoxx4kidsfr.github.io/materials/stylesheets/styles.css
+GHCSS2=devoxx4kidsfr.github.io/materials/stylesheets/pygment_trac.css
+
 for mdfile in $(find ./ateliers -name "*.md" -anewer $(dirname $0)/gen-docs-timestamp)
 do
     echo $mdfile
-   pandoc $mdfile --from=markdown --to=html5 --standalone --number-sections --output=$(dirname $mdfile)/$(basename $mdfile .md).html
+   pandoc $mdfile --from=markdown --to=html5 --standalone --css $GHCSS1 --css $GHCSS2 --output=$(dirname $mdfile)/$(basename $mdfile .md).html
 done
+
+
 
 touch $(dirname $0)/gen-docs-timestamp
 
