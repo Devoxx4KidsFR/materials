6b53dddb4770c01cc5ce967b7b516026ef267329
Claude Falguiere <cfalguiere@gmail.com>
2015-05-23 16:57:41 +0200

    déplacement des explications sur la photorésistante

--- ateliers/arduino/01-Facile/FM03-Le thérémine/Theremine/Theremine.ino
+++ ateliers/arduino/01-Facile/FM03-Le thérémine/Theremine/Theremine.ino
@@ -10,16 +10,16 @@ void setup() {
 }
 
 void loop() {
-  // lit la valeur sur le pin AO
+  // Lit la valeur sur le connecteur AO
   int lumiere = analogRead(A0);
-  // S'il se bloque sur l'ordinateur, on peut lire la valeur
+  // on peut lire la valeur sur l'ordinateur
   Serial.println(lumiere);
-  // la valeur de lumiere change selon l'endroit, on suppose qu'elle est entre 400 et 1000 
-  // nous voulons que l'instrument joue une frequence entre 120 Hz et 800 Hz
-  // map traduit converti les valeurs 400-100 0 en valeurs 120-800
-  int frequence = map(lumiere, 400, 1000, 120, 800);
+  // La valeur de lumiere change selon l'endroit,  peu pres entre 400 et 1000. 
+  // Nous voulons que l'instrument joue une frequence entre 132 Hz (Do2) et 990 Hz Si4) 
+  // map converti les valeurs 400-1000 en valeurs 132-990
+  int frequenceNote = map(lumiere, 400, 1000, 120, 990);
   // joue la note
-  tone(PIN_BUZZER, frequence, dureeNote);
+  tone(PIN_BUZZER, frequenceNote, dureeNote);
   // une très courte pause donne un meilleur résultat
   delay(1);        
 }
